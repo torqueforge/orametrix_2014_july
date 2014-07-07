444238f0e7109c4211ccb68e6b4a70f484139092
Phil Getto <phil.getto@orametrix.com>
2014-07-07 09:57:12 +0200

    Ex 1.

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,6 +1,35 @@
 # Feel free to delete the instructions once you get going
-puts instructions(__FILE__)
+# puts instructions(__FILE__)
 
 class House
+  def recite
+    %q[This is the house that Jack built.
 
+This is the malt that lay in the house that Jack built.
+
+This is the rat that ate the malt that lay in the house that Jack built.
+
+This is the cat that killed the rat that ate the malt that lay in the house that Jack built.
+
+This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+
+This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+
+This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+
+This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+
+This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+
+This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+
+This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+
+This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+]
+  end
+
+  def line(line_number)
+    recite.split("\n")[2*(line_number-1)]+"\n"
+  end
 end
