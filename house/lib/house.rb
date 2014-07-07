8b607d768d89c0a58e583a780e001ac5bf3ff7a0
Wilfried Sperber <wilfried.sperber@orametrix.de>
2014-07-07 13:09:00 +0200

    commit

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -3,7 +3,6 @@ puts instructions(__FILE__)
 
 class House
   TALE = [
-      "",
       "This is the house that Jack built.\n",
       "This is the malt that lay in the house that Jack built.\n",
       "This is the rat that ate the malt that lay in the house that Jack built.\n",
@@ -18,12 +17,10 @@ class House
       "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
   ]
   def line number
-     TALE[number]
+     TALE[number-1]
   end
 
   def recite
-    TALE.each_with_index do |line, index = 1|
-      line index
-    end
+    TALE.join("\n")
   end
 end
