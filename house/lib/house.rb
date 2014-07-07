ca9a5669f33dad2a7ec09b7912f358c31d99c074
localadmin <localadmin@linux-hans.orametrix.de>
2014-07-07 11:10:12 +0200

    change it again

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -3,27 +3,28 @@ puts instructions(__FILE__)
 
 class House
 
-def lines
-  @lines = []
-  @lines << "This is the house that Jack built.\n"
-  @lines << "This is the malt that lay in the house that Jack built.\n"
-  @lines << "This is the rat that ate the malt that lay in the house that Jack built.\n"
-  @lines << "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-  @lines << "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-  @lines << "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-  @lines << "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-  @lines << "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-  @lines << "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-  @lines << "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-  @lines << "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-  @lines << "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-  @lines
-end
+  def lines
+    @lines ||= [
+      "This is the house that Jack built.\n",
+      "This is the malt that lay in the house that Jack built.\n",
+      "This is the rat that ate the malt that lay in the house that Jack built.\n",
+      "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+      "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+      "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+      "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+      "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+      "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+      "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+      "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+      "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+    ]
+  end
 
-def line line_number
-  lines[line_number-1]
-end
-def recite
-  lines.join("\n")
-end
+  def line(line_number)
+    lines[line_number-1]
+  end
+
+  def recite
+    lines.join("\n")
+  end
 end
