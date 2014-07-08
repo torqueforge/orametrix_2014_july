9f095f545e95a0be3d6238b6c6c6e89da988ed9f
parkranger <localadmin@linux-hans.orametrix.de>
2014-07-08 14:33:02 +0200

    master backup

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,31 +1,43 @@
 class Bottles
-  def verse(num)
-    case num
-    when (3..99)
-    "#{num} bottles of beer on the wall, #{num} bottles of beer.
-Take one down and pass it around, #{num-1} bottles of beer on the wall.
-"
-    when 2
-"2 bottles of beer on the wall, 2 bottles of beer.
-Take one down and pass it around, 1 bottle of beer on the wall.
-"
-
-    when 1
-"1 bottle of beer on the wall, 1 bottle of beer.
-Take it down and pass it around, no more bottles of beer on the wall.
-"
-    when 0
-"No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.
-"
-    end
+  def song
+    verses(99, 0)
   end
 
   def verses(high, low)
     high.downto(low).collect { |num| verse(num) }.join("\n")
   end
 
-  def song
-    verses(99, 0)
+  def verse(num)
+    case num
+    when 0
+      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    else
+      "#{num} #{container(num)} of beer on the wall, #{num} #{container(num)} of beer.\nTake #{pronoun(num)} down and pass it around, #{amount(num-1)} #{container(num-1)} of beer on the wall.\n"
+    end
+  end
+
+private
+  def container(count)
+    if count == 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
+
+  def pronoun(count)
+    if count == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def amount(num)
+    if num == 0
+      "no more"
+    else
+      num
+    end
   end
 end
